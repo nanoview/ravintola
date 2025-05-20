c14b6f8d5613dceb1d6062280a0135e48a92f6b7
nanoview <arif.js@gmail.com>
2025-05-20 17:03:37 +0300

    ...

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import Layout from '../components/Layout';
 import { useToast } from '@/hooks/use-toast';
-import { supabase } from '@/lib/supabase';
+import { supabase } from '@/integrations/supabase/client';
 import { 
   Table, 
   TableHeader, 
